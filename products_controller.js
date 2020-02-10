6ea46a9074bec182d7dda29f10314336a181ed86
Traci Crawford <tracicrawford@Tracis-MacBook-Air.local>
2020-02-10 14:09:50 -0600

    Node 3 Completed

---
+++ products_controller.js
@@ -0,0 +1,52 @@
+module.exports = {
+    create: (req, res, next) => {
+        const dbInstance = req.app.get('db')
+        const {name, description, price, image_url} = req.body
+
+        dbInstance.create_product([name, description, price, image_url])
+        .then( () => res.sendStatus(200))
+        .catch(err => {
+            res.status(500).send({errorMessage: "An error has occurred"})
+            console.log(err)
+    })
+    },
+    getOne: (req, res, next) => {
+        const dbInstance = req.app.get('db')
+        const {id} = req.params
+
+        dbInstance.read_product(id).then (product => res.status(200).send(product))
+        .catch (err => {
+            res.status(500).send({errorMessage: "An error has occurred"})
+            console.log(err)
+        })
+    },
+    getAll: (req, res, next) => {
+        const dbInstance = req.app.get('db')
+
+        dbInstance.read_products().then (products => res.status(200).send(products))
+        .catch (err => {
+            res.status(500).send ({errorMessage: "An error has occurred"})
+            console.log(err)
+        })
+    },
+    update: (req, res, next) => {
+        const dbInstance = req.app.get('db') 
+        const {params, query} = req
+
+        dbInstance.update_product([params.id, query.desc]).then( () => res.sendStatus(200))
+        .catch (err => {
+            res.status(500).send ({errorMessage: "An error has occurred"})
+            console.log(err)
+        })
+    },
+    delete: (req, res, next) => {
+        const dbInstance = req.app.get('db')
+        const {id} = req.params
+
+        dbInstance.delete_product(id).then (() => res.sendStatus(200))
+        .catch (err => {
+            res.status(500).send({errorMessage: "An error has occurred"})
+            console.log(err)
+        })
+    }
+}
